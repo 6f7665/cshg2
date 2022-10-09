69d534584b5bead8ad2a63eb93ba3d644e0c4f03
Oscar Vestlund <ove@lysator.liu.se>
2022-10-09 20:24:25 +0200

    main.cpp: add main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <filesystem>
+#include <iostream> //cout
+#include <ncurses.h> //ncurses
+#include <string>
+#include <thread>
+#include <vector> //dynamic arrays
+
+//self coded:
+
+
+//namespaces:
+using namespace std;
+namespace fs = std::filesystem;
+
+int main(int argc, char** argv)
+{
+
+}
